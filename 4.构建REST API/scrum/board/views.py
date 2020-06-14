57c711041a0b635caf6a272ac373b1e149cfaac2
luxxcn <953817@qq.com>
2020-06-14 13:04:36 +0800

    构建REST API

---
+++ 4.构建REST API/scrum/board/views.py
@@ -0,0 +1,68 @@
+# from django.shortcuts import render
+
+from django.contrib.auth import get_user_model
+
+from rest_framework import authentication, permissions, viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from .forms import TaskFilter, SprintFilter
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+User = get_user_model()
+
+class DefaultMixin(object):
+    """Default setting for view authentication, permissions, filtering and pagination."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size',
+    max_paginate_by = 100
+    filter_backends = (
+        # filters.DjangoFilterBackend,
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+class SprintViewSet(DefaultMixin, viewsets.ModelViewSet):
+    """API endpoint fro listing and creating sprints."""
+
+    """
+    IDE报错：Class 'Sprint' has no 'objects' member
+    需要对ide进行配置
+    如：vscode 添加配置 User Settings (Ctrl + , or File > Preferences > Settings
+    "python.linting.pylintArgs": [
+        "--load-plugins=pylint_django",
+    ]
+    """
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+    filter_class = SprintFilter
+    search_fields = ('name', )
+    ordering_fields = ('end', 'name', )
+
+class TaskViewSet(DefaultMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_class = TaskFilter
+    search_fields = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
+
+class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
+    """API endpoint for listing users."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
+    search_fields = (User.USERNAME_FIELD, )
